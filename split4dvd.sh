0053df55e4cb072fd86594bab6197ed16ff2cb42
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2019-01-29 20:17:21 -0600

    split4dvd super smarter

--- split4dvd.sh
+++ split4dvd.sh
@@ -6,9 +6,13 @@ fi
 # to reformat DVD
 # yum -y install udftools
 # mkudffs /dev/sr0
-# 3.2 GB
-split -b 3221225472 "${1}" "${1}_"
+# 3.2 GB, my DVD is a little messed up?
+#split -b 3221225472 "${1}" "${1}_"
 # 4.2 GB
 #split -b 4509715660 "${1}" "${1}_"
 #split -b 4689887232 "${1}" "${1}_"
 #split -b 4702989189 "${1}" "${1}_"
+# figure out the bytes necessary for splitting: 
+# 4 GB
+size=$(echo "4 * 1024 * 1024 * 1024" | bc)
+split -b $size "${1}" "${1}_"
