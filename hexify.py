e12eb293c1c3b9e4d0a61174fdb605984db3664f
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2019-06-06 09:23:02 -0500

    hexify CLI

--- hexify.py
+++ hexify.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Thu May 30 09:30:28 2019
 
 @author: DivollHJ
 """
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('usage : hexify -u <unhex files/dirs> -h <hex files/dirs>')
+        print('may use both -u and -h in same command')
+        sys.exit(0)
 import os
 from binascii import hexlify, unhexlify
 
 exthex = hexlify('EXTENSION')
 
 def hexify(fname):
+    print(fname)
     with open(fname,'r') as fid:
         binry = hexlify(fid.read())
     fn,ext = os.path.splitext(fname)
@@ -62,4 +70,43 @@ def unhexifyall(dname):
                 os.rename('{}{}'.format(os.path.splitext(thefile)[0],ext),os.path.join(newdir,newsubdir,'{}{}'.format(os.path.splitext(os.path.basename(thefile))[0],ext)))
             else:
                 os.rename('{}{}'.format(os.path.splitext(thefile)[0],ext),os.path.join(newdir,'{}{}'.format(os.path.splitext(os.path.basename(thefile))[0],ext)))
+
+if __name__ == '__main__':
+    DUN = False
+    NUN = False
+    filez = {'DUN':[],'NUN':[]}
+    dirz  = {'DUN':[],'NUN':[]}
+    for arg in sys.argv[1:]:
+        print(arg)
+        if arg.startswith('-'):
+            if arg == '-h':
+                DUN = False
+                NUN = True
+            elif arg == '-u':
+                DUN = True
+                NUN = False
+            else:
+                print('Unknown ARG : '+arg)
+                sys.exit(1)
+            continue
+        cp = os.path.realpath(arg)
+        if DUN:
+            if os.path.isdir(cp):
+                dirz['DUN'].append(cp)
+            elif os.path.isfile(cp):
+                dirz['NUN'].append(cp)
+        elif NUN:
+            if os.path.isdir(cp):
+                filez['DUN'].append(cp)
+            elif os.path.isfile(cp):
+                filez['NUN'].append(cp)
+    for xfile in filez['DUN']:
+        unhexify(xfile)
+    for xfile in dirz['DUN']:
+        unhexifyall(xfile)
+    for xfile in filez['NUN']:
+        hexify(xfile)
+    for xfile in dirz['NUN']:
+        hexifyall(xfile)
+        
     
