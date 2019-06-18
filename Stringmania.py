80b82c702b895ae8d75598dfd907f6bec8a5f279
Carl-Analytix <41123776+Carl-Analytix@users.noreply.github.com>
2019-06-17 22:11:02 -0500

    For Hamilton
    Uses a series of regular expressions to fix shit data with lots of random spaces and quotes everywhere.  x is a list of the resulting words.

---
+++ Stringmania.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 19:53:39 2019
+
+@author: Jordan
+"""
+import re
+
+string = "This      is a '   long ' string with '            '    '               '  super  annoying spaces in 'between     '      'hey     '       '     '"
+#Replace whitespace in quotes after a word with just the word "'hello     '" -> "'hello'"
+newstring = re.sub(r"('(\s+|)[A-Za-z]+)[\s]{1,}'",r"\1'",string)
+#Replace whitespace in quotes before a word with just the word "'     hello'" -> "'hello'"
+newstring = re.sub(r"'[\s]{1,}([A-Za-z]+')",r"'\1",newstring)
+#Replace multiple whitespace with one space "'     '" -> "' '"
+newstring = re.sub(" +"," ",newstring)
+newstring = re.sub(" ' '"," ' '  ",newstring)
+#Make something unique to set the unwanted quotes to HHH is used.
+newstring = re.sub(" ' ' "," HHH'   'HHH ",newstring)
+#Replace quotes that have more than 2 spaces in them with the item you want
+# HHH'        'HHH -> ' FML '
+newstring = re.sub("HHH'  +'HHH",' FML ',newstring)
+#Replace things with one ore more spaces with just one space
+# The quick      brown    fox ->  The quick brown fox
+newstring = re.sub(" +"," ",newstring)
+
+#Get rid of the quotes finally
+newstring = re.sub("'","",newstring)
+#Split on the spaces
+x = newstring.split()
