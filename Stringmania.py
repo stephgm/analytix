a47d134924220c3d343b8ea868cd945df2698d4a
Carl-Analytix <41123776+Carl-Analytix@users.noreply.github.com>
2019-06-25 22:29:06 -0500

    String mania update
    Found out that there are some sentences within quotes... couldn't handle that before, now should be okay.. everything from before is commented out

--- Stringmania.py
+++ Stringmania.py
@@ -5,12 +5,21 @@ Created on Mon Jun 17 19:53:39 2019
 @author: Jordan
 """
 import re
+import time
 
-string = "This      is a '   long ' string with '            '    '               '  super  annoying spaces in 'between     '      'hey     '       '     '"
+start = time.time()
+
+string = "This      is a '   long ' 'string with this one  ' 'this should be taken care of' '            '    '               '  super  annoying spaces in 'between     '      'hey     '       '     '"
+for quoted_part in re.findall(r"\'(.+?)\'", string):
+    string = string.replace(quoted_part, quoted_part.replace(" ", ""))
 #Replace whitespace in quotes after a word with just the word "'hello     '" -> "'hello'"
-newstring = re.sub(r"('(\s+|)[A-Za-z]+)[\s]{1,}'",r"\1'",string)
-#Replace whitespace in quotes before a word with just the word "'     hello'" -> "'hello'"
-newstring = re.sub(r"'[\s]{1,}([A-Za-z]+')",r"'\1",newstring)
+#newstring = re.sub(r"('(\s+|)[A-Za-z]+)[\s]{1,}'",r"\1'",string)
+##Replace whitespace in quotes before a word with just the word "'     hello'" -> "'hello'"
+#newstring = re.sub(r"'[\s]{1,}([A-Za-z]+')",r"'\1",newstring)
+#Fix the empty quotes '' to ' '
+newstring = re.sub(r"''",r"'   '",string)
+##replace whitespace inside quotes
+#newstring = re.sub(r"'([A-Za-z]+)\s+([A-Za-z]+)\s+'",r"'\1\2'",newstring)
 #Replace multiple whitespace with one space "'     '" -> "' '"
 newstring = re.sub(" +"," ",newstring)
 newstring = re.sub(" ' '"," ' '  ",newstring)
@@ -18,7 +27,7 @@ newstring = re.sub(" ' '"," ' '  ",newstring)
 newstring = re.sub(" ' ' "," HHH'   'HHH ",newstring)
 #Replace quotes that have more than 2 spaces in them with the item you want
 # HHH'        'HHH -> ' FML '
-newstring = re.sub("HHH'  +'HHH",' XXX ',newstring)
+newstring = re.sub("HHH'  +'HHH",' FML ',newstring)
 #Replace things with one ore more spaces with just one space
 # The quick      brown    fox ->  The quick brown fox
 newstring = re.sub(" +"," ",newstring)
@@ -28,5 +37,6 @@ newstring = re.sub("'","",newstring)
 #Split on the spaces
 x = newstring.split()
 for i,item in enumerate(x):
-    if item == 'XXX':
+    if item == 'FML':
         x[i] = ''
+print time.time() - start
