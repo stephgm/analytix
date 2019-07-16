74afcabbdbdb58093f1803926356ce71611c3ddb
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2019-07-15 19:51:30 -0500

    fixer_script.sh update

--- fixer_script.sh
+++ fixer_script.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
-for i in $(cat iplist)
+this=$(echo ${HOSTNAME} | awk -F "." '{print $1}')
+echo $this
+
+sharez=()
+
+for i in $(cat machine_shares)
 do
-ip=$(echo $i | awk -F "," '{print $1}')
-first=$(echo $i | awk -F "," '{print $2}')
-last=$(echo $i | awk -F "," '{print $3}')
+that=$(echo $i | awk -F "," '{print $1}')
+if [ "${that}" == "${this}" ];then
+share=$(echo $i | awk -F "," '{print $2}')
+echo $share
+sharez+=("$share")
+fi
 done
 
+echo "#" > /etc/exports
+
+for i in $(cat iplist.csv)
+do
+ip=$(echo $i | awk -F "," '{print $1}')
+mach=$(echo $i | awk -F "," '{print $2}')
+if ! [ "${mach}" == "${this}" ];then
+for ((j=0;j<${#sharez[@]};j++));do
+echo "${sharez[j]}""       ${ip}""/255.255.255.0(rw,sync,no_root_squash)" >> /etc/exports
+done
+fi
+#last=$(echo $i | awk -F "," '{print $3}')
+done
+exportfs -r
+exit
+############################################################
 # OR
 un=()
 while IFS= read -r line
@@ -22,6 +46,5 @@ newips=()
 for ((i=0;i<${#newips[@]};i++));do
 echo "/storage/share       ${newips[i]}/255.255.255.0(rw,sync,no_root_squash)" >> /etc/exports
 done
-exportfs -r
 
 # /etc/hosts
