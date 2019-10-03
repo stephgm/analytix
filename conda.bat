3b17075bcfb8e1809d9d3567a9835922066beb83
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2019-10-03 09:58:44 -0500

    move

--- conda.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set PATH=c:\windows\systems32;c:\anaconda3;c:\anaconda3\Library\usr\bin;c:\anaconda3\Library\bin;c:\anaconda3\Scripts;%PATH%
-c:\anaconda3\Scripts\conda %*
-endlocal
