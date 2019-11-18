e79c9ad84a19615f3fb7f4b5916d628dffcc0e80
Carl-Analytix <41123776+Carl-Analytix@users.noreply.github.com>
2019-11-17 21:07:42 -0600

    Update TreeWidget.py

--- treewidget/TreeWidget.py
+++ treewidget/TreeWidget.py
@@ -83,6 +83,83 @@ class TreeModelTest(QMainWindow):
                 data = data[key]
             
         return data
+    
+    class LayeredTree(Widgets.QTreeWidget):
+        checkChanged = Core.pyqtSignal(object)
+        unCheckedChanged = Core.pyqtSignal(object)
+        
+        def __init__(self,parent=None,structure={},**kwargs):
+            super(LayeredTree,self).__init__(parent)
+            
+            self.checkRoot = kwargs.get('checkRoot',False)
+            self.allChecks = kwargs.get('allChecks',False)
+            self.addChecks = kwargs.get('addChecks',True)
+            
+            self.parent = parent
+            self.structure = structure
+            self.makeTree()
+            self.previousSelection = {}
+            
+            self.itemChanged.connect(lambda trash:self.getChecked())
+            
+        def makeTree(self):
+            if isinstance(self.structure,dict):
+                def walkDict(d,parent):
+                    for k, v in d.items():
+                        child = Widgets.QTreeWidgetItem(parent)
+                        child.setText(0,k)
+                        child.setCheckState(0, Core.Qt.Unchecked)
+                        if isinstance(v, dict):
+                            if self.addChecks:
+                                child.setFlags(child.flags() | Core.Qt.ItemIsTristate | Core.Qt.ItemIsUserCheckable)
+                            v = walkDict(v,child)
+                        else:
+                            if self.addChecks:
+                                child.setFlags(child.flags() | Core.Qt.ItemIsUserCheckable)
+                   
+                for key in self.structure:  
+                    parent = Widgets.QTreeWidgetItem(self)
+                    parent.setText(0,key)
+                    if self.checkRoot and self.addChecks:
+                        parent.setFlags(parent.flags() | Core.Qt.ItemIsTristate | Core.Qt.ItemIsUserCheckable)
+                        parent.setCheckState(0,Core.Qt.Unchecked)
+                    if isinstance(self.structure[key],dict):
+                        walkDict(self.structure[key],parent)
+                    # else:
+                    #     if self.addChecks:
+                    #         parent.setFlags(parent.flags() | Core.Qt.ItemIsTristate | Core.Qt.ItemIsUserCheckable)
+        
+        def getChecked(self):
+            if self.addChecks:
+                paths = []
+                newpaths = []
+                for i in range(self.topLevelItemCount()):
+                    item = self.topLevelItem(i)
+                    def walkItems(treeitem):
+                        if treeitem.childCount():
+                            for j in range(treeitem.childCount()):
+                                child = treeitem.child(j)
+                                walkItems(child)
+                        else:
+                            if treeitem.checkState(0) != Core.Qt.Unchecked:
+                                hasparent = True
+                                path = treeitem.text(0)
+                                parent = treeitem.parent()
+                                while hasparent:
+                                    if not parent:
+                                        hasparent = False
+                                    else:
+                                        path = parent.text(0)+'/'+path
+                                        parent = parent.parent()
+                                paths.append(path)
+                        
+                    walkItems(item)
+                if paths:
+                    newpaths = []
+                    for path in paths:
+                        newpaths.append(path.split('/'))
+                    print(newpaths)
+                return newpaths
                 
 def main():
     app = QApplication(sys.argv)
