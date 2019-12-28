a58bea071c41a0b932b0ffd47a094d57d0e78afc
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2019-12-27 21:12:32 -0600

    a little winderz progress but still some work on adar config

---
+++ adarConfigurator/run.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;c:\anaconda3-b4;c:\anaconda3-b4\Library\usr\bin;c:\anaconda3-b4\Library\bin;c:\anaconda3-b4\Scripts;c:\Program Files\7-zip
+adarConfigurator.exe
+endlocal
