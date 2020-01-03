6f6ed56e8136caf5eb20f15c3341bde85b5fc3ca
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2020-01-03 08:52:43 -0600

    pip py3 augment3 update-pip fixes and additions

--- pip.bat
+++ pip.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set PATH=c:\windows\systems32;c:\anaconda3;c:\anaconda3\Library\usr\bin;c:\anaconda3\Library\bin;c:\anaconda3\Scripts;%PATH%
+set PATH=c:\windows\systems32;c:\anaconda3-b4;c:\anaconda3-b4\Library\usr\bin;c:\anaconda3-b4\Library\bin;c:\anaconda3-b4\Scripts;%PATH%
 set PYTHONHTTPSVERIFY=0
-c:\anaconda3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org %*
+c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org %*
 endlocal
--- update-pips.bat
+++ update-pips.bat
@@ -8,6 +8,6 @@ set TABULATE_INSTALL=lib-only
 c:\anaconda3-b4\Scripts\conda.exe update --all -y
 c:\anaconda3-b4\Scripts\conda.exe clean -a -y
 
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install --upgrade msgpack argparse urwid construct hexdump pypcapfile python-pcapng avro python-pptx orderedset objgraph pandastable altair altair-widgets seaborn-altair altair-recipes yerkes gencharts actdiag blockdiag nwdiag seqdiag arrow dill pathlib terminal-table tabulate pyinstaller
-
+c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install --upgrade msgpack argparse urwid construct hexdump pypcapfile python-pcapng avro python-pptx orderedset objgraph pandastable altair altair-widgets seaborn-altair altair-recipes yerkes gencharts actdiag blockdiag nwdiag seqdiag arrow dill pathlib terminal-table tabulate pyinstaller removestar flynt
+@pause
 endlocal
