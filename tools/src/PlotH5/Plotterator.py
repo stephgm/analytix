60b92f049605108637a66f869d94f36bf8c6bfdb
Carl-Analytix <41123776+Carl-Analytix@users.noreply.github.com>
2020-01-10 09:40:52 -0600

    syncing with Highside

--- tools/src/PlotH5/Plotterator.py
+++ tools/src/PlotH5/Plotterator.py
@@ -353,7 +353,7 @@ class Plotter(object):
             except:
                 self.lines = []
             for rowcol in self.sub:
-                for line in self.sub['lines']:
+                for line in self.sub[rowcol]['lines']:
                     self.lines.append(line)
             # handling for versions that did not track colorbars separately
             try:
@@ -453,7 +453,7 @@ class Plotter(object):
                         for command in self.sub[t]['commands']:
                             if command['cmd'].startswith('twin'):
                                 execString = 'thisax.'+self.buildExecString(command)
-                                othAx[t] = eval(execString,(),{'thisax':theaxes[rowcol]})
+                                othAx[t] = eval(execString,{},{'thisax':theaxes[rowcol]})
                 cbs = [(rc,self.sub[rc]['colorbar']['colorbarname'],self.sub[rc]['colorbar']['cbardata']) for rc in rowcols
                        if len(rc) == 3 and rc[:2] == rowcol and self.sub[rc]['colorbar'] and 'colorbarname' in self.sub[rc]['colorbar']]
                 for rc,cb,dt in cbs:
@@ -687,7 +687,7 @@ class Plotter(object):
                          **{k:line[k] for k in line if k not in exclude_list})
         elif line['plottype'] == 'plot3d':
             sc = ax.plot(line['x'],line['y'],line['z'],line['fmt'],
-                         **{k:line[k] for k in line if k not in exclude_list})
+                         **{k:line[k] for k in line if k not in exclude_list})[-1]
         elif line['plottype'] == 'scatter':
             sc = ax.scatter(line['x'],line['y'],
                             **{k:line[k] for k in line if k not in exclude_list})
@@ -723,7 +723,7 @@ class Plotter(object):
             return axid
         else:
             print('Axes location already initiated')
-            return []
+            return axid
     
     def plot(self,x,y,fmt='',axid=(0,0),**kwargs):
         '''
