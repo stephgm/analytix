38e87103745bb79a9de3dfe98f8beb947624adbc
Carl-Analytix <41123776+Carl-Analytix@users.noreply.github.com>
2020-03-04 21:09:08 -0600

    Updated for the mappings.  Added kwargs

--- parseIDMP.py
+++ parseIDMP.py
@@ -14,9 +14,10 @@ from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-
+import string
 import time
 
+CapAlphabet = string.ascii_uppercase
 
 def read_IDMP(fpath,**kwargs):
     '''
@@ -31,13 +32,17 @@ def read_IDMP(fpath,**kwargs):
         get_all - True: Returns all the DIDs
                   False:  Returns the DIDs that follow our criteria - no "pre" no "ee"
                   TODO-UPDATE THESE BECAUSE I CANNOT RECALL ALL OF THEM
+        get_mapping - True: Returns the **** mapping ONLY
+                      False: Returns the list of DIDs ONLY
     Returns:
         Returns a list of DIDs expected for the event
     '''
 
     get_all = kwargs.get('get_all',True)
+    get_mapping = kwargs.get('get_mapping',False)
     lines = []
     DIDs = []
+    mapdict = {'OSF':{},'TPY':{},'THAAD':{}}
     ladd = lines.append
     if os.path.isfile(fpath) and os.path.splitext(fpath)[-1] == '.pdf' and 'IDMP' in fpath:
         file_content = open(fpath,'rb')
@@ -75,13 +80,34 @@ def read_IDMP(fpath,**kwargs):
                 break
         if first_data_items and last_data_items:
             RELEVANT_TEXT = lines[first_data_items:last_data_items]
-
+            #This part parses the DID names only
             DIDs = list(map(lambda x:x.split(' ')[0],RELEVANT_TEXT))
             if get_all:
                 DIDs = [d for d in DIDs if '-' in d and 'gti' not in d.lower()]
             else:
                 DIDs = [d for d in DIDs if '-' in d and 'gti' not in d.lower() and
                         not '-pre-' in d.lower() and not d.lower().startswith('ee')]
+            #This part maps the first two numbers after the '-' in the DID to the numbers in the ()
+            for line in lines:
+                DID = line.split(' ')[0]
+                if '-' in DID and 'gti' not in DID.lower():
+                    if '(' in line and ')...' in line and 'osf-' in DID.lower() and '-pre-' not in DID.lower():
+                        value = line.split('(')[-1].split(')')[0]
+                        if value[-1] in CapAlphabet:
+                            value = value[:-1]
+                        key = DID.split('-')[-1][:2]
+                        mapdict['OSF'][key] = value
+                    if '(' in line and ')...' in line and ('tpy2-' in DID.lower() or 'typ2-' in DID.lower()) and '-pre-' not in DID.lower():
+                        value = line.split('(')[-1].split(')')[0]
+                        key = DID.split('-')[-1]
+                        if key[-1] in CapAlphabet:
+                            key = key[-1]
+                        mapdict['TPY'][key] = value
+                    if '(' in line and ')...' in line and 'thaad' in DID.lower() and '-pre-' not in DID.lower():
+                        value = line.split('(')[-1].split(')')[0]
+                        value = 'String' + value.split('String')[-1]
+                        key = DID.split('-')[-1][:2]
+                        mapdict['THAAD'][key] = value
         else:
             print('Could not find the relevant information needed to parse..  Returning []')
         file_content.close()
@@ -90,22 +116,30 @@ def read_IDMP(fpath,**kwargs):
               1: not a valid file\n
               2: not a pdf\n
               3: the file does not contain the string 'IDMP'. ''')
-
-    return DIDs
+    if not get_mapping:
+        return DIDs
+    else:
+        return mapdict
 
 
 if __name__ == '__main__':
     # fpath = '/home/klinetry/Desktop/GTI-07b_EC_Draft_2_IDMP.pdf'
-    start = time.time()
-    z = {}
-    for path in map(os.path.realpath,sys.argv[1:]):
-        if os.path.isdir(path):
-            for infile in glob.glob(os.path.join(path,'*.pdf')):
-                print(infile)
-                z[os.path.basename(infile)] = read_IDMP(infile,get_all=True)
-    # jj = read_IDMP(fpath,get_all=True)
-    print(time.time() - start)
-    jj = []
-    for key in z:
-        jj.extend(z[key])
-    jj = sorted(map(str.lower,set(jj)))
+    if True:
+        start = time.time()
+        fpath = '/home/klinetry/Desktop/GTI-20_Sprint_2_(JEON)_Draft_3_IDMP.pdf'
+        jj = read_IDMP(fpath, get_mapping=True)
+        print(time.time()-start)
+    if False:
+        start = time.time()
+        z = {}
+        for path in map(os.path.realpath,sys.argv[1:]):
+            if os.path.isdir(path):
+                for infile in glob.glob(os.path.join(path,'*.pdf')):
+                    print(infile)
+                    z[os.path.basename(infile)] = read_IDMP(infile,get_all=True)
+        # jj = read_IDMP(fpath,get_all=True)
+        print(time.time() - start)
+        jj = []
+        for key in z:
+            jj.extend(z[key])
+        jj = sorted(map(str.lower,set(jj)))
