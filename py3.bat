e56d18cc7d80cea1c13a85e6edfcbb0ff275c495
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2020-03-08 12:30:16 -0500

    bat file updates

--- py3.bat
+++ py3.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-set PATH=c:\windows\systems32;c:\anaconda3-b4;c:\anaconda3-b4\Library\usr\bin;c:\anaconda3-b4\Library\bin;c:\anaconda3-b4\Scripts;%PATH%
-c:\anaconda3-b4\python.exe -W ignore %*
+set PATH=c:\windows\systems32;c:\anaconda3-2019.10b3;c:\anaconda3-2019.10b3\Library\usr\bin;c:\anaconda3-2019.10b3\Library\bin;c:\anaconda3-2019.10b3\Scripts;%PATH%
+c:\anaconda3-2019.10b3\python.exe -W ignore %*
 endlocal
