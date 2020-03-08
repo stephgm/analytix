a0353f0259b343560909949c631e82957372a0f8
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2020-03-08 12:25:00 -0500

    installer updates and added junkyWinderzInstall bat file

--- anaconda_building/augment3.bat
+++ anaconda_building/augment3.bat
@@ -1,42 +1,42 @@
 @echo off
 setlocal
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;c:\anaconda3-b4;c:\anaconda3-b4\Library\usr\bin;c:\anaconda3-b4\Library\bin;c:\anaconda3-b4\Scripts;c:\Program Files\7-zip
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;c:\anaconda3-2019.10b3;c:\anaconda3-2019.10b3\Library\usr\bin;c:\anaconda3-2019.10b3\Library\bin;c:\anaconda3-2019.10b3\Scripts;c:\Program Files\7-zip
 set PYTHONHTTPSVERIFY=0
 set TABULATE_INSTALL=lib-only
 :: for unit testing
-:: c:\anaconda3-b4\Scripts\conda.exe install -y pyculib
+:: c:\anaconda3-2019.10b3\Scripts\conda.exe install -y pyculib
 :: exit
-c:\anaconda3-b4\Scripts\conda.exe config --set ssl_verify false
-c:\anaconda3-b4\Scripts\conda.exe update -n base conda
-c:\anaconda3-b4\Scripts\conda.exe update --all -y
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install --upgrade pip
-c:\anaconda3-b4\Scripts\conda.exe install -y R cartopy swig pyopengl pyopengl-accelerate pyqtgraph traitlets vispy hdf4 pymssql boost cudatoolkit cmake line_profiler cython cairo pyopengl pyopengl-accelerate plotly autopep8 geopandas selenium mock nodejs holoviews datashader hvplot graphviz panel param xmltodict
-c:\anaconda3-b4\Scripts\conda.exe install -y -c spyder-ide spyder=4.0.1
+c:\anaconda3-2019.10b3\Scripts\conda.exe config --set ssl_verify false
+c:\anaconda3-2019.10b3\Scripts\conda.exe update -n base conda
+c:\anaconda3-2019.10b3\Scripts\conda.exe update --all -y
+c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install --upgrade pip
+c:\anaconda3-2019.10b3\Scripts\conda.exe install -y R cartopy swig pyopengl pyopengl-accelerate pyqtgraph traitlets vispy hdf4 pymssql boost cudatoolkit cmake line_profiler cython cairo pyopengl pyopengl-accelerate plotly autopep8 geopandas selenium mock nodejs holoviews datashader hvplot graphviz panel param xmltodict
+c:\anaconda3-2019.10b3\Scripts\conda.exe install -y -c spyder-ide spyder=4.0.1
 :: failures : pyculib
 :: glib gobject-introspection
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install msgpack argparse urwid
+c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install msgpack argparse urwid
 :: what actually works
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install construct hexdump pypcapfile python-pcapng avro python-pptx pyinstaller removestar flynt ipytree
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install pyopencl pycuda
+c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install construct hexdump pypcapfile python-pcapng avro python-pptx pyinstaller removestar flynt ipytree
+c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install pyopencl pycuda
 rem dont work
-rem c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install altair altair-widgets seaborn-altair altair-recipes yerkes gencharts
+rem c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install altair altair-widgets seaborn-altair altair-recipes yerkes gencharts
 :: John Weier's addtitions
-c:\anaconda3-b4\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install actdiag blockdiag nwdiag seqdiag arrow colorcet dill pathlib terminal-table tabulate removestar flynt pdfminer3k
-c:\anaconda3-b4\Scripts\conda.exe clean -a -y
-c:\anaconda3-b4\python.exe c:\tools\src\feature_download.py --output \anaconda3-b4\Lib\site-packages\cartopy\data physical
-c:\anaconda3-b4\python.exe c:\tools\src\feature_download.py --output \anaconda3-b4\Lib\site-packages\cartopy\data physical
-c:\anaconda3-b4\python.exe c:\tools\src\feature_download.py --output \anaconda3-b4\Lib\site-packages\cartopy\data cultural
-c:\anaconda3-b4\python.exe c:\tools\src\feature_download.py --output \anaconda3-b4\Lib\site-packages\cartopy\data cultural-extra
-c:\anaconda3-b4\python.exe c:\tools\src\feature_download.py --output \anaconda3-b4\Lib\site-packages\cartopy\data gshhs
+c:\anaconda3-2019.10b3\python -m pip --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org install actdiag blockdiag nwdiag seqdiag arrow colorcet dill pathlib terminal-table tabulate removestar flynt pdfminer3k
+c:\anaconda3-2019.10b3\Scripts\conda.exe clean -a -y
+c:\anaconda3-2019.10b3\python.exe c:\tools\src\feature_download.py --output \anaconda3-2019.10b3\Lib\site-packages\cartopy\data physical
+c:\anaconda3-2019.10b3\python.exe c:\tools\src\feature_download.py --output \anaconda3-2019.10b3\Lib\site-packages\cartopy\data physical
+c:\anaconda3-2019.10b3\python.exe c:\tools\src\feature_download.py --output \anaconda3-2019.10b3\Lib\site-packages\cartopy\data cultural
+c:\anaconda3-2019.10b3\python.exe c:\tools\src\feature_download.py --output \anaconda3-2019.10b3\Lib\site-packages\cartopy\data cultural-extra
+c:\anaconda3-2019.10b3\python.exe c:\tools\src\feature_download.py --output \anaconda3-2019.10b3\Lib\site-packages\cartopy\data gshhs
 cd C:\tools\natural_earth
-mkdir C:\anaconda3-b4\Lib\site-packages\cartopy\data\shapefiles\natural_earth
-robocopy . C:\anaconda3-b4\Lib\site-packages\cartopy\data\shapefiles\natural_earth /s /e
+mkdir C:\anaconda3-2019.10b3\Lib\site-packages\cartopy\data\shapefiles\natural_earth
+robocopy . C:\anaconda3-2019.10b3\Lib\site-packages\cartopy\data\shapefiles\natural_earth /s /e
 cd ..
-copy  NE1_HR_LC_SR_W_DR.png C:\anaconda3-b4\Lib\site-packages\cartopy\data\raster\natural_earth
+copy  NE1_HR_LC_SR_W_DR.png C:\anaconda3-2019.10b3\Lib\site-packages\cartopy\data\raster\natural_earth
 cd c:\tools
 7z x ViTables-3.0.0.tar 
 cd ViTables-3.0.0
-c:\anaconda3-b4\python setup.py install
+c:\anaconda3-2019.10b3\python setup.py install
 cd ..
 rmdir /s /q ViTables-3.0.0
 @pause
---
+++ anaconda_building/junkyWindowsInstall.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
+set TMP="c:\tools\tmp"
+set TEMP="c:\tools\tmp"
+\tools\installers\Anaconda3-2019.10-Windows-x86_64.exe
+endlocal
