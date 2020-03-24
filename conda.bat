fcced7d7179b5c4703ff38bfacef09cce3d9c812
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2020-03-24 14:23:55 -0500

    added conda.bat

---
+++ conda.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set PATH=c:\windows\systems32;c:\anaconda3-2020.02b1;c:\anaconda3-2020.02b1\Library\usr\bin;c:\anaconda3-2020.02b1\Library\bin;c:\anaconda3-2020.02b1\Scripts;%PATH%
+c:\anaconda3-2020.02b1\Scripts\conda.exe %*
+endlocal
