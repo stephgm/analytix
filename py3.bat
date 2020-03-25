f75c26fff09dc404c345e754f3f4d8cd72107774
Hamilton Holliday <hamilton.holliday@analytix-hsv.com>
2020-03-25 10:05:42 -0500

    b2 bat files

--- py3.bat
+++ py3.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
-set PATH=c:\windows\systems32;c:\anaconda3-2020.02b1;c:\anaconda3-2020.02b1\Library\usr\bin;c:\anaconda3-2020.02b1\Library\bin;c:\anaconda3-2020.02b1\Scripts;%PATH%
-c:\anaconda3-2020.02b1\python.exe -W ignore %*
+set PATH=c:\windows\systems32;c:\anaconda3-2020.02b2;c:\anaconda3-2020.02b2\Library\usr\bin;c:\anaconda3-2020.02b2\Library\bin;c:\anaconda3-2020.02b2\Scripts;%PATH%
+c:\anaconda3-2020.02b2\python.exe -W ignore %*
 endlocal
--- spyder.bat
+++ spyder.bat
@@ -2,7 +2,7 @@
 setlocal
 REM set PATH=c:\windows\systems32;c:\anaconda2;c:\anaconda2\Library\usr\bin;c:\anaconda2\Library\bin;c:\anaconda2\Scripts;%PATH%
 REM c:\anaconda2\python.exe c:\anaconda2\Scripts\spyder-script.py
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;c:\anaconda3-2020.02b1;c:\anaconda3-2020.02b1\Library\usr\bin;c:\anaconda3-2020.02b1\Library\bin;c:\anaconda3-2020.02b1\Scripts;%PATH%
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;c:\anaconda3-2020.02b2;c:\anaconda3-2020.02b2\Library\usr\bin;c:\anaconda3-2020.02b2\Library\bin;c:\anaconda3-2020.02b2\Scripts;%PATH%
 
-c:\anaconda3-2020.02b1\python.exe c:\anaconda3-2020.02b1\Scripts\spyder-script.py
+c:\anaconda3-2020.02b2\python.exe c:\anaconda3-2020.02b2\Scripts\spyder-script.py
 endlocal
